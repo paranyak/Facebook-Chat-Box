888d7ac7c96718992864c43078f5be4b1e857d97
paranyak <paraniack.n@gmail.com>
2018-01-20 12:54:03 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,30 @@ module.exports = {
     entry: './scripts/index.js',
     output: {
         filename: './build/bundle.js'
+    },
+    module: {
+
+
+        rules: [{
+            test: /\.less$/,
+            use: [
+
+
+                {
+                    loader: "style-loader" // creates style nodes from JS strings
+                },
+                {
+                    // loader: "css-loader!less-loader",
+                    loader: "css-loader" // translates CSS into CommonJS
+                },
+                {
+                    // loader: "css-loader!less-loader",
+                    loader: "less-loader" // compiles Less to CSS
+                },
+
+
+            ]
+        },
+        ],
     }
 };
