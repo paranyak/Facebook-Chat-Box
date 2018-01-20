b475c7dd5f99e08368b2b3c114416615c91c9387
paranyak <paranyak@ucu.edu.ua>
2018-01-20 13:53:48 +0200

    Fixed bug with text input

--- build/bundle.js
+++ build/bundle.js
@@ -67,9 +67,18 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-
 const formMessageCreation = document.querySelectorAll(".chat-box");
 
+
+
+const escapeHtml = (unsafe)  => {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+};
 
 for (let i = 0; i < formMessageCreation.length; i++) {
     formMessageCreation[i].addEventListener('keydown', (evt) => {
@@ -80,23 +89,29 @@ for (let i = 0; i < formMessageCreation.length; i++) {
         let usersText = messages[messages.length - 1].querySelectorAll(".message__your-text");
 
         if (evt.keyCode === 13) {
+            let inputText = formMessageCreation[i].querySelector(".create-message__text").value;
             evt.preventDefault();
-            console.log(usersText);
-            if(formMessageCreation[i].querySelector(".create-message__text").value.length>0){
-            if (usersText.length === 1) {
-                usersText[0].classList.add('message__your-text_first');
-            } else {
-                usersText[usersText.length - 1].classList.add('message__your-text_middle');
-                usersText[usersText.length - 1].classList.remove('message__your-text_last');
+            inputText = escapeHtml(inputText);
+            if (inputText.length > 0) {
+                if (usersText.length === 1) {
+                    usersText[0].classList.add('message__your-text_first');
+                } else {
+                    usersText[usersText.length - 1].classList.add('message__your-text_middle');
+                    usersText[usersText.length - 1].classList.remove('message__your-text_last');
+                }
+                let item = `<p class="message__your-text message__your-text_last">${inputText}</p>`;
+                if (inputText.length < 30) {
+                    item = `<p class="message__your-text message__your-text_small message__your-text_last">${inputText}</p>`;
+                }
+                messages[messages.length - 1].innerHTML += item;
+
+                messages[messages.length - 1].scrollTop = messages[messages.length - 1].scrollHeight;
+                let elem = formMessageCreation[i].querySelector(".chat");
+                elem.scrollTop = elem.scrollHeight;
+                chatBoxFirst.reset();
             }
-            let item = `<p class="message__your-text message__your-text_last">${formMessageCreation[i].querySelector(".create-message__text").value}</p>`;
-            if (document.querySelector(".create-message__text").value.length < 30) {
-                item = `<p class="message__your-text message__your-text_small message__your-text_last">${formMessageCreation[i].querySelector(".create-message__text").value}</p>`;
-            }
-            messages[messages.length - 1].innerHTML += item;
-            chatBoxFirst.reset();
-        }}
-        if(evt.keyCode === 27){
+        }
+        if (evt.keyCode === 27) {
             chatBoxFirst.reset();
         }
     }, false);
