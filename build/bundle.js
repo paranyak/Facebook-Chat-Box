08c43ee5ca372b7570f848661b6f004615b85777
paranyak <paranyak@ucu.edu.ua>
2018-01-20 13:24:40 +0200

    Fixed bug with text input

--- build/bundle.js
+++ build/bundle.js
@@ -67,37 +67,7 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-// const formMessageCreation = document.querySelector(".create-message");
-//
-// const addMessage = (evt) => {
-//     let messages = document.querySelectorAll(".message_you");
-//
-//     let usersText = messages[messages.length - 1].querySelectorAll(".message__your-text");
-//
-//     if (evt.keyCode === 13) {
-//         evt.preventDefault();
-//         console.log(usersText);
-//         if (usersText.length === 1) {
-//             usersText[0].classList.add('message__your-text_first');
-//         } else {
-//             usersText[usersText.length - 1].classList.add('message__your-text_middle');
-//             usersText[usersText.length - 1].classList.remove('message__your-text_last');
-//         }
-//         let item = `<p class="message__your-text message__your-text_last">${document.querySelector(".create-message__text").value}</p>`;
-//         if (document.querySelector(".create-message__text").value.length < 30) {
-//             item = `<p class="message__your-text message__small-text message__your-text_last">${document.querySelector(".create-message__text").value}</p>`;
-//         }
-//         messages[messages.length - 1].innerHTML += item;
-//         formMessageCreation.reset();
-//     }
-// };
-//
-//
-// formMessageCreation.addEventListener('keypress', addMessage, false);
-//
-//
-//
-//
+
 const formMessageCreation = document.querySelectorAll(".chat-box");
 
 
@@ -121,7 +91,7 @@ for (let i = 0; i < formMessageCreation.length; i++) {
             }
             let item = `<p class="message__your-text message__your-text_last">${formMessageCreation[i].querySelector(".create-message__text").value}</p>`;
             if (document.querySelector(".create-message__text").value.length < 30) {
-                item = `<p class="message__your-text message__small-text message__your-text_last">${formMessageCreation[i].querySelector(".create-message__text").value}</p>`;
+                item = `<p class="message__your-text message__your-text_small message__your-text_last">${formMessageCreation[i].querySelector(".create-message__text").value}</p>`;
             }
             messages[messages.length - 1].innerHTML += item;
             chatBoxFirst.reset();
