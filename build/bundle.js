467f0ee758691fe4e56d7489ea41ebdb9d3f61f3
paranyak <paranyak@ucu.edu.ua>
2018-01-20 16:24:06 +0200

    Fixed bugs

--- build/bundle.js
+++ build/bundle.js
@@ -67,59 +67,59 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-// const formMessageCreation = document.querySelectorAll(".chat-box");
-//
-//
-//
-// const escapeHtml = (unsafe)  => {
-//     return unsafe
-//         .replace(/&/g, "&amp;")
-//         .replace(/</g, "&lt;")
-//         .replace(/>/g, "&gt;")
-//         .replace(/"/g, "&quot;")
-//         .replace(/'/g, "&#039;");
-// };
-//
-// for (let i = 0; i < formMessageCreation.length; i++) {
-//     formMessageCreation[i].addEventListener('keydown', (evt) => {
-//         let chatBoxFirst = formMessageCreation[i].querySelector(".create-message");
-//
-//         let messages = formMessageCreation[i].querySelectorAll(".message_you");
-//
-//         let usersText = messages[messages.length - 1].querySelectorAll(".message__your-text");
-//
-//         if (evt.keyCode === 13) {
-//             let inputText = formMessageCreation[i].querySelector(".create-message__text").value;
-//             evt.preventDefault();
-//             inputText = escapeHtml(inputText);
-//             if (inputText.length > 0) {
-//                 if (usersText.length === 1) {
-//                     usersText[0].classList.add('message__your-text_first');
-//                 } else {
-//                     usersText[usersText.length - 1].classList.add('message__your-text_middle');
-//                     usersText[usersText.length - 1].classList.remove('message__your-text_last');
-//                 }
-//                 let item = `<p class="message__your-text_last">${inputText}</p>`;
-//                 if (inputText.length < 30) {
-//                     item = `<p class="message__your-text_small message__your-text_last">${inputText}</p>`;
-//                 }
-//                 messages[messages.length - 1].innerHTML += item;
-//
-//                 messages[messages.length - 1].scrollTop = messages[messages.length - 1].scrollHeight;
-//                 let elem = formMessageCreation[i].querySelector(".chat");
-//                 elem.scrollTop = elem.scrollHeight;
-//                 chatBoxFirst.reset();
-//             }
-//         }
-//         if (evt.keyCode === 27) {
-//             chatBoxFirst.reset();
-//         }
-//     }, false);
-// }
-//
-//
-//
-//
+const formMessageCreation = document.querySelectorAll(".chat-box");
+
+
+
+const escapeHtml = (unsafe)  => {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+};
+
+for (let i = 0; i < formMessageCreation.length; i++) {
+    formMessageCreation[i].addEventListener('keydown', (evt) => {
+        let chatBoxFirst = formMessageCreation[i].querySelector(".create-message");
+
+        let messages = formMessageCreation[i].querySelectorAll(".message_you");
+
+        let usersText = messages[messages.length - 1].querySelectorAll("p");
+
+        if (evt.keyCode === 13) {
+            let inputText = formMessageCreation[i].querySelector(".create-message__text").value;
+            evt.preventDefault();
+            inputText = escapeHtml(inputText);
+            if (inputText.length > 0) {
+                if (usersText.length === 1) {
+                    usersText[0].classList.add('message__your-text_first');
+                } else {
+                    usersText[usersText.length - 1].classList.add('message__your-text_middle');
+                    usersText[usersText.length - 1].classList.remove('message__your-text_last');
+                }
+                let item = `<p class=" message__your-text_last">${inputText}</p>`;
+                if (inputText.length < 30) {
+                    item = `<p class=" message__your-text_small message__your-text_last">${inputText}</p>`;
+                }
+                messages[messages.length - 1].innerHTML += item;
+
+                messages[messages.length - 1].scrollTop = messages[messages.length - 1].scrollHeight;
+                let elem = formMessageCreation[i].querySelector(".chat");
+                elem.scrollTop = elem.scrollHeight;
+                chatBoxFirst.reset();
+            }
+        }
+        if (evt.keyCode === 27) {
+            chatBoxFirst.reset();
+        }
+    }, false);
+}
+
+
+
+
 
 
 /***/ })
